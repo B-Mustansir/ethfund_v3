09337228669b8ebf56cf9709d81f1024a57d5a12
Mustansir Bohari <mustansirzain2@gmail.com>
2023-08-03 22:41:36 +0530

    static

--- ethfund/settings.py
+++ ethfund/settings.py
@@ -142,7 +142,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/templates/html/'
 STATICFILES_DIRS=(os.path.join(BASE_DIR,'templates/html'),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 
