7cde3892a68c80d8f2b33654312fb2aa3ba493f4
Mustansir Bohari <mustansirzain2@gmail.com>
2023-08-03 20:52:05 +0530

    d2

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 echo "BUILD START"
-python -m pip install -r requirements.txt --no-cache-dir
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput --clear
+python3.9 -m pip install -r requirements.txt --no-cache-dir
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD END"
